32468e92df6ef5a0719bdf2a0373178659dbdc51
Maximilian Karasz <maximilian.karasz@stylefruits.de>
2013-07-11 14:37:04 +0200

    test clojure version change.

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/mknoszlig/mini-jmx"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :aot :all)
