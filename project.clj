c1fcedd4fbb615a0e6ee94348e399455de1fe194
Maximilian Karasz <maximilian.karasz@stylefruits.de>
2013-07-11 13:43:07 +0200

    fix project url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mini-jmx "0.1.0-SNAPSHOT"
   :description "a minimal drop-in jmx connector"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/mknoszlig/mini-jmx"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
